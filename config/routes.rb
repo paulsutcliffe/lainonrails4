49785394824fbade39bfd1c9edca1ab7296e431b
Jesus <jesussayes@gmail.com>
2014-07-04 18:58:07 -0500

    Setup simple_form. Remove Admin and Rol models. User.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  devise_for :admins, :controllers => { :registrations => "admins/registrations" }, :path => "admins", :path_names => { :sign_in => 'ingresar', :sign_out => 'salir', :password => 'secreto', :confirmation => 'verificacion', :unlock => 'desbloquear', :registration => 'registro', :sign_up => 'inscribirse' }
+  devise_for :users
   resources :ads
 
 end
