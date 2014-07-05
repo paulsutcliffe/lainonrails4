d36bd1f1504a44f3d844f307819b416f55465ef0
Jesus <jesussayes@gmail.com>
2014-07-04 20:16:11 -0500

    Rails layout bootstrap3. Ability. Role. User & Role seeds. User: Basic factory.

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,6 +1,20 @@
-# Read about factories at https://github.com/thoughtbot/factory_girl
+require 'faker'
 
 FactoryGirl.define do
   factory :user do
+    email                 { Faker::Internet.email }
+    password              { "12345678" }
+    password_confirmation { "12345678" }
+
+    trait :admin do
+      email "admin@example.com"
+      after(:create) {|user| user.add_role(:admin)}
+    end
+
+    trait :user do
+      email "user@example.com"
+      after(:create) {|user| user.add_role(:user)}
+    end
   end
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
-describe User, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+describe User do
+  it "has a valid factory" do
+    expect(build(:user)).to be_valid
+  end
 end
