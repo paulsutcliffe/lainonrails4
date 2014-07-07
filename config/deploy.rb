9078e59c7970385564ca7af6fd75e959d6de9dab
Paul Sutcliffe <paul@kosmyka.com>
2014-07-07 12:32:46 -0500

    repository

--- config/deploy.rb
+++ config/deploy.rb
@@ -2,7 +2,7 @@
 lock '3.2.1'
 
 set :application, 'my_app_name'
-set :repo_url, 'git@github.com:paul/lainonrails4.git'
+set :repo_url, 'git@github.com:paulsutcliffe/lainonrails4.git'
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
