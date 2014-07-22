ad1bcfe48e6ddb31048fbd6a45f84492a86b7b48
Paul Sutcliffe <paul@kosmyka.com>
2014-07-21 21:56:23 -0500

    menu styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def menu_status(item)
+    if params[:id] == item
+      @menu_status = 'active'
+    else
+      if request.path_parameters[:controller] == item
+        @menu_status = 'active'
+      else
+        @menu_status = 'inactive'
+      end
+    end
+  end
 end
