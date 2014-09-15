c6940f720f2fb85bf85f71cbf55cb72917a0d2cd
Paul Sutcliffe <paul@kosmyka.com>
2014-09-15 11:15:27 -0500

    no redirection

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -2,20 +2,20 @@ class CountriesController < ApplicationController
 
   def index
     @country_code = request.location.country_code
-    case @country_code
-    when "PE"
-      redirect_to '/pe/'
-    when "BO"
-      redirect_to '/bo/'
-    when "CR"
-      redirect_to '/cr/'
-    when "GT"
-      redirect_to '/gt/'
-    when "SV"
-      redirect_to '/sv/'
-    when "UY"
-      redirect_to '/uy/'
-    end
+    # case @country_code
+    # when "PE"
+    #   redirect_to '/pe/'
+    # when "BO"
+    #   redirect_to '/bo/'
+    # when "CR"
+    #   redirect_to '/cr/'
+    # when "GT"
+    #   redirect_to '/gt/'
+    # when "SV"
+    #   redirect_to '/sv/'
+    # when "UY"
+    #   redirect_to '/uy/'
+    # end
   end
 
 end
