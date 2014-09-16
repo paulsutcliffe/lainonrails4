fcf114997f750187fd2ee027817d47a9927f7fb4
Jesus <jesussayes@gmail.com>
2014-09-15 20:31:08 -0500

    Countries: Redirection patch.

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -1,21 +1,25 @@
 class CountriesController < ApplicationController
 
   def index
-    @country_code = request.location.country_code
-    # case @country_code
-    # when "PE"
-    #   redirect_to '/pe/'
-    # when "BO"
-    #   redirect_to '/bo/'
-    # when "CR"
-    #   redirect_to '/cr/'
-    # when "GT"
-    #   redirect_to '/gt/'
-    # when "SV"
-    #   redirect_to '/sv/'
-    # when "UY"
-    #   redirect_to '/uy/'
-    # end
+    # @country_code = request.location.country_code
+    remote_ip     = request.remote_ip
+    data_response = HTTParty.get("http://freegeoip.net/json/#{remote_ip}")
+    @country_code = JSON.parse(data_response.body)["country_code"]
+    # Rails.logger.info "#{data_response}"
+    case @country_code
+    when "PE"
+      redirect_to '/pe/'
+    when "BO"
+      redirect_to '/bo/'
+    when "CR"
+      redirect_to '/cr/'
+    when "GT"
+      redirect_to '/gt/'
+    when "SV"
+      redirect_to '/sv/'
+    when "UY"
+      redirect_to '/uy/'
+    end
   end
 
 end
