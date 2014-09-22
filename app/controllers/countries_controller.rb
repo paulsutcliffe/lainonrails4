0ee1abcff609de497afb4549622463bf6b36d518
Paul Sutcliffe <paul@kosmyka.com>
2014-09-22 11:33:50 -0500

    ready

--- app/controllers/countries_controller.rb
+++ app/controllers/countries_controller.rb
@@ -6,31 +6,24 @@ class CountriesController < InheritedResources::Base
   skip_before_filter :set_variables
 
   def index
-    # @country_code = request.location.country_code
-    # remote_ip     = request.remote_ip
     remote_ip = request.env['REMOTE_ADDR']
     data_response = HTTParty.get("http://freegeoip.net/json/#{remote_ip}")
     @country_code = JSON.parse(data_response.body)["country_code"]
     Rails.logger.info "#{data_response}"
-    if @country_code == 'PE'
+    case @country_code
+    when "PE"
       redirect_to '/pe/'
-    else
-      redirect_to en_construccion_path
+    when "BO"
+      redirect_to '/bo/'
+    when "CR"
+      redirect_to '/cr/'
+    when "GT"
+      redirect_to '/gt/'
+    when "SV"
+      redirect_to '/sv/'
+    when "UY"
+      redirect_to '/uy/'
     end
-    # case @country_code
-    # when "PE"
-    #   redirect_to '/pe/'
-    # when "BO"
-    #   redirect_to '/bo/'
-    # when "CR"
-    #   redirect_to '/cr/'
-    # when "GT"
-    #   redirect_to '/gt/'
-    # when "SV"
-    #   redirect_to '/sv/'
-    # when "UY"
-    #   redirect_to '/uy/'
-    # end
   end
 
   private
